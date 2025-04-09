bf221f6f82efba3ebdbc7b0323259484a8b714ba
Himanshu Gupta <maxsevengit@users.noreply.github.com>
2025-04-10 00:14:23 +0530

    added inbox

---
+++ pages/inbox.php
@@ -0,0 +1,9 @@
+<?php
+require_once('../includes/auth.php');
+require_once('../includes/header.php');
+?>
+
+<h2>Inbox</h2>
+<p>Your messages and notifications will appear here.</p>
+
+<?php require_once('../includes/footer.php'); ?>
