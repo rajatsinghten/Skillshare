3558eebd61de79610647ed98a0204763226ac14e
Himanshu Gupta <maxsevengit@users.noreply.github.com>
2025-04-10 00:14:54 +0530

    added index

---
+++ pages/index.php
@@ -0,0 +1,7 @@
+<?php require_once('../includes/header.php'); ?>
+
+<h1>Welcome to SkillShare</h1>
+<p>Find and share amazing skills with others.</p>
+<a href="register.php">Register</a> | <a href="login.php">Login</a>
+
+<?php require_once('../includes/footer.php'); ?>
