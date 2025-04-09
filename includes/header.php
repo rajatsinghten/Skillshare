ac2bd9adc6bb501270feff25243bd1cafce796b7
Rajat Singh <rajatsinghten@gmail.com>
2025-04-10 03:07:35 +0530

    navbar fix

--- includes/header.php
+++ includes/header.php
@@ -5,13 +5,18 @@
     <link rel="stylesheet" href="../assets/css/style.css">
 </head>
 <body>
-    <header>
-        <h1>Skill Share</h1>
-        <nav>
-            <a href="../pages/dashboard.php">Dashboard</a> |
-            <a href="../pages/post_skill.php">Post Skill</a> |
-            <a href="../pages/search.php">Search</a> |
-            <a href="../logout.php">Logout</a>
-        </nav>
+    <header class="site-header">
+        <div class="container header-content">
+            <h1 class="logo">SkillShare</h1>
+            <nav class="main-nav">
+                <a href="../pages/dashboard.php">Dashboard</a>
+                <a href="../pages/post_skill.php">Post Skill</a>
+                <a href="../pages/my_skills.php">My Skills</a>
+                <a href="../pages/inbox.php">Inbox</a>
+                <a href="../pages/search.php">Search</a>
+                <a href="../logout.php">Logout</a>
+            </nav>
+        </div>
     </header>
     <hr>
+
